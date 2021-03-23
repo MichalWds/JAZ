0b95e613f2c7b6ccb652419861847e2068340529
Michal Wadas <michal.wadas@hycom.pl>
2021-03-23 17:44:32 +0100

    first

---
+++ src/main/java/pl/pjatk/micwad/MicwadApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.micwad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicwadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicwadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/micwad/MicwadApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.micwad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicwadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

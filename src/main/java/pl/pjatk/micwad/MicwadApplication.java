c17aba1e6f1daf353100fa307b301b71161c2355
Michal Wadas <michal.wadas@hycom.pl>
2021-03-28 16:45:26 +0200

    fix importResources

--- src/main/java/pl/pjatk/micwad/MicwadApplication.java
+++ src/main/java/pl/pjatk/micwad/MicwadApplication.java
@@ -2,15 +2,16 @@ package pl.pjatk.micwad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
+@ImportResource({"classpath:beans.xml"})
 public class MicwadApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicwadApplication.class, args);
-        new ClassPathXmlApplicationContext("beans.xml");
+      //  new ClassPathXmlApplicationContext("beans.xml");
 //		ComponentTwo componentTwo = (ComponentTwo) context.getBean("componentTwo");
 //		componentTwo.helloFromMethod();
     }

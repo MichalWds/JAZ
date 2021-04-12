d7f4f010c41435ace4efe1e92441836bbec9709a
Michal Wadas <michal.wadas@hycom.pl>
2021-04-12 22:50:44 +0200

    test

--- src/main/java/pl/pjatk/micwad/MySimpleClass.java
+++ src/main/java/pl/pjatk/micwad/MySimpleClass.java
@@ -3,6 +3,7 @@ package pl.pjatk.micwad;
 public class MySimpleClass {
 
     private final String name;
+    private String ass;
 
     public MySimpleClass(String name) {
         this.name = name;

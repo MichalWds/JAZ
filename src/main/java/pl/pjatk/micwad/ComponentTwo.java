5f39fef27fdd2e320f27322aea53de6ed8040246
Michal Wadas <michal.wadas@hycom.pl>
2021-04-16 13:23:14 +0200

    praca domowa z 2 zjazdu, properties value yml

--- src/main/java/pl/pjatk/micwad/ComponentTwo.java
+++ src/main/java/pl/pjatk/micwad/ComponentTwo.java
@@ -1,15 +1,28 @@
 package pl.pjatk.micwad;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.w3c.dom.ls.LSOutput;
 
 @Component
 public class ComponentTwo {
 
     public ComponentTwo(@Value("${my.custom.property}") String valueFromProperties,
+                  //      @Value("${my.custom.someHomeworkValue}") boolean someHomeworkValue,
                         @Value("${my.custom.random:myDefaultValue}") String randomValue) {
         System.out.println(valueFromProperties);
         System.out.println(randomValue);
+        System.out.println("----------------");
+      //  System.out.println(someHomeworkValue);
+        System.out.println("----------------");
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "my.custom.someHomeworkValue", havingValue = "true")
+    public void homeworkString(){
+        System.out.println("PLACKI Z JABLKAMI SA MEGA DOBRE, A VALUE = TRUE, WIEC ODPALAM TEGO BEANA");;
     }
 
     void helloFromMethod() {
---
+++ src/main/java/pl/pjatk/micwad/SomeHomeworkValue.java
@@ -0,0 +1,9 @@
+package pl.pjatk.micwad;
+
+public class SomeHomeworkValue {
+    private String someHomeworkValue;
+
+    public SomeHomeworkValue(String someHomeworkValue) {
+        this.someHomeworkValue = someHomeworkValue;
+    }
+}
